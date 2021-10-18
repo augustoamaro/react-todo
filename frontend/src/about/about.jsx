ca199275657cc8778d07dbe7bc5c7f6b83ad1ec8
Augusto <augustoamaro@icloud.com>
2021-10-18 19:53:41 -0300

    Created template

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Página feita com react</h2>
+        <p>API em Node.js</p>
     </div>
 )
