aa1bfb5de42a380d245bbbef4186255ab407d653
Augusto <augustoamaro@icloud.com>
2021-10-20 17:40:56 -0300

    Changes in frontend and backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,9 +4,16 @@ const express = require('express');
 const server = express();
 const allowCors = require('./cors')
 
+server.use(allowCors);
+
+server.use('*', (req, res, next) => {
+    const request = `[${req.method}] - ${req.hostname}${req.path} ${req.ip}`;
+
+    console.log(request);
+    next();
+});
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
-server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`);
