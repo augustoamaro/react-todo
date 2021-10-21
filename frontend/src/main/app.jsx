879ced7ae8fbf8756dfb0dc79a5d91d9c263725a
Augusto <augustoamaro@icloud.com>
2021-10-21 11:18:20 -0300

    Finished todo work flow

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,6 +1,7 @@
 
-require('bootstrap/dist/css/bootstrap.min.css')
-require('font-awesome/css/font-awesome.min.css')
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+import '../template/custom.css'
 
 import React from 'react'
 import Menu from '../template/menu'
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import IconButton from "../template/iconButton";
 
 export default props => {
 
     const renderRows = () => {
-        return (
-            <tr><td>ok</td></tr>
-        )
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+                <td>
+                    <IconButton style='success' icon='check' hide={todo.done}
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                    <IconButton style='warning' icon='undo' hide={!todo.done}
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
+                        onClick={() => props.handleRemove(todo)}></IconButton>
+                </td>
+            </tr>
+        ))
 
     }
     return (
@@ -13,6 +25,7 @@ export default props => {
             <thead>
                 <tr>
                     <th>Descrição</th>
+                    <th>Ações</th>
                 </tr>
             </thead>
             <tbody>
